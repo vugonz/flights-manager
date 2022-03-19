337639a1be9b87962c33bbe921b18291b4f18aa3
itsVugz <82618983+itsVugz@users.noreply.github.com>
2022-03-19 19:41:20 +0000

    tweak the tester

--- test.sh
+++ test.sh
@@ -8,24 +8,21 @@ fi
 bin="${1}"			# executable
 test_dir="${2}"		# tests folder
 
-if [[ $# -eq 2 ]]; then
-	bin="${1}"			# executable
+for t in $test_dir/*.in; do
+	if [[ -f $t ]]; then
+		./$bin < $t > ${t%.in}.myout
+	fi
+done
 
-	for t in $test_dir/*.in; do
-		if [[ -f $t ]]; then
-			./$bin < $t > ${t%.in}.myout
-		fi
-	done
+for t in $test_dir/*.myout; do
+	diff -c $t ${t%.myout}.out > ${t%.myout}.diff 
+done
 
-	for t in $test_dir/*.myout; do
-		diff -c $t ${t%.myout}.out > ${t%.myout}.diff 
-	done
+for t in $test_dir/*.diff; do
+	echo "${t:(-11):(-5)} result"
+	cat $t
+	echo $'\n'
+done
 
-	for t in $test_dir/*.diff; do
-		echo "${t:(-11):(-5)} result"
-		cat $t
-		echo $'\n'
-	done
+rm $test_dir/*.myout $test_dir/*.diff
 
-	rm $test_dir/*.myout $test_dir/*.diff
-fi
