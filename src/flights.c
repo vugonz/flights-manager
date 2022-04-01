0b240e4dcc48529ca2ebb201de6a0d4fcd1dbe84
itsVugz <82618983+itsVugz@users.noreply.github.com>
2022-04-01 13:01:41 +0000

    odiasoidjasoidj

--- src/flights.c
+++ src/flights.c
@@ -29,27 +29,32 @@ int add_flight(manager *system, char *id, char *origin, char *destination,
 	if(!exists_airport_id(system, destination))
 		return -4;
 
-	/* if max flights limit reached */
+	/* check if max flights limit reached */
 	if(system->nr_flights >= MAX_FLIGHTS)
 		return -5;
 	
+	/* check if flight date is a year or less into the future */
 	if(!is_valid_date(departure.date, system->date))
 		return -6;
 
 	if(!is_valid_duration(duration))
 		return -7;
 	
-	/* check if plane capacity is met in range [10,100] */
+	/* check if flight capacity is met in range [10,100] */
 	if(nr_passengers < MIN_FLIGHT_CAPACITY || nr_passengers > MAX_FLIGHT_CAPACITY)
 		return -8;
 
+	/* create new flight */
 	new_flight = create_flight(id, origin, destination, 
 			departure, duration, nr_passengers);
 
+	/* insert new flight in system's sorted lists */
 	insert_flight(system, new_flight);
 
+	/* increment nr of flights in origin airport */
 	++get_airport_by_id(system, origin)->nr_flights;
 
+	/* increment global nr of flights */
 	++system->nr_flights;
 
 	return 0;
@@ -80,6 +85,7 @@ void insert_sorted_flight(flight *l, flight new_flight, int size, int (*cmp_func
 	
 	l[size] = new_flight;
 
+	/* inserts new flight in sorted position using cmp_func as comparison method */
 	for(i = size - 1; i >= 0; --i) {
 		if(cmp_func(new_flight, l[i]) > 0) {
 			l[i+1] = new_flight;
@@ -113,9 +119,11 @@ int list_flights_by_airport(manager *system, char *airport_id, char command)
 	if(!exists_airport_id(system, airport_id))
 		return -1;
 	
+	/* if given command is 'p' */
 	if(command == DEPARTURE) {
 		list_airport_flights_by_departure(system->sorted_departure_flights, 
 				airport_id, system->nr_flights);
+	/* if given command is 'c' */
 	} else
 		list_airport_flights_by_arrival(system->sorted_arrival_flights, 
 				airport_id, system->nr_flights);
@@ -130,6 +138,7 @@ void list_airport_flights_by_departure(flight *l, char *airport_id, int size)
 {
 	int i;
 
+	/* print flights in sorted by departure schedule array if they have given airport_id as origin */
 	for(i = 0; i < size; ++i)
 		if(!strcmp(l[i].origin, airport_id))
 			print_flight_in_airport(l[i].id, l[i].destination, l[i].departure);
@@ -142,6 +151,7 @@ void list_airport_flights_by_arrival(flight *l, char *airport_id, int size)
 {
 	int i;
 
+	/* print flights in sorted by arrival schedule array if they have given airport_id as destination */
 	for(i = 0; i < size; ++i)
 		if(!strcmp(l[i].destination, airport_id))
 			print_flight_in_airport(l[i].id, l[i].origin, l[i].arrival);
@@ -162,6 +172,7 @@ flight create_flight(char *id, char *origin, char *destination,
 	new_flight.nr_passengers = nr_passengers;
 
 	new_flight.departure = departure;
+	/* calculate arrival schedule for flight */
 	new_flight.arrival = calculate_arrival(departure, duration);
 
 	return new_flight;
@@ -196,6 +207,7 @@ int is_valid_flight_id(char *id)
 	char c1, c2;
 	int n;
 
+	/* check if two first characters are upper case and if remaining characters form an integer in ]0, 9999]*/
 	if(sscanf(id,"%c%c%d", &c1, &c2, &n) == 3) 
 		return isupper(c1) && isupper(c2) && n > 0 && n < FLIGHT_MAX_NUM_ID ? 1 : 0;
 	
