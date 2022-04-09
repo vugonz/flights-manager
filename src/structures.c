4e3775adfcc66bc864b6aba175319414e3e1e3a3
itsVugz <82618983+itsVugz@users.noreply.github.com>
2022-04-09 23:37:28 +0000

    ??

--- src/structures.c
+++ src/structures.c
@@ -78,3 +78,76 @@ reservation *find_node_in_list(list *l, char *id)
 	return p;
 }
 
+/*
+ * Sorts given list of reservations in alphabetical order of reservation's id using mergesort
+ */
+reservation *sort_list(reservation *head, int size)
+{
+	int i;
+	reservation *left = head;
+	reservation *right = head;
+	reservation *aux;
+	
+	/* empty list case or base case for recursive calls */
+	if(size == 0 || size == 1)
+		return head;
+
+	/* get right pointing to middle list's element */
+	for(i = 0; i < size - size/2 - 1; ++i)
+		right = right->next;
+	/* divide list */
+	aux = right->next;
+	right->next = NULL;
+	right = aux;
+
+	/* recursive division */
+	left = sort_list(left, size - size/2);
+	right = sort_list(right, size/2);
+	/* return pointer of sorted sub division to previous stack */
+	return merge(left, right);
+}
+
+reservation *merge(reservation *h1, reservation *h2)
+{
+	reservation *aux;  /* auxiliary pointer to traverse */
+	reservation *head; /* head of sorted list */
+	
+	/* get first element in auxiliary pointer */
+	aux = strcmp(h1->id, h2->id) < 0 ? h1 : h2;
+	if(aux == h1)
+		h1 = h1->next;
+	else
+		h2 = h2->next;
+	
+	/* save head pointer */
+	head = aux;
+
+	/* link elements in order until a sub list runs out of elements */
+	while(h1 != NULL && h2 != NULL) {
+		if(strcmp(h1->id, h2->id) < 0) {
+			aux->next = h1;
+			h1 = h1->next;
+		} else {
+			aux->next = h2;
+			h2 = h2->next;
+		}
+		aux = aux->next;
+	}
+
+	/* link remaining elements */
+	if(h1 != NULL) {
+		while(h1 != NULL) {
+			aux->next = h1;
+			h1 = h1->next;
+			aux = aux->next;
+		}
+	} else {
+		while(h2 != NULL) {
+			aux->next = h2;
+			h2 = h2->next;
+			aux = aux->next;
+		}
+	}
+	return head;
+}
+
